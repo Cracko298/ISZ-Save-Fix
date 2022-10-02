147653553c9429caf25692b9234f7593daa7433f
Yunix <97799691+TheRealYunix@users.noreply.github.com>
2022-10-02 22:22:47 +0200

    Update Plugin.cpp

--- v0.5/SaveFixISZ/Plugin.cpp
+++ v0.5/SaveFixISZ/Plugin.cpp
@@ -1,4 +1,6 @@
+#include "RepairCrash.hpp"
 #include "FileDialog.hpp"
+
 #include <fstream>
 #include <string>
 
@@ -11,60 +13,24 @@ bool inRange(unsigned low, unsigned high, unsigned x)
 namespace Plugin
 {
 	void WriteAndReadFromPlugin(const int &PluginFileSeek, const int &MinimumValue, const int &MaximumValue, const int &NewValue, const char SelectedData[6])
-	{
+	{ 
+			std::string EndInfo;
+			int ReadedData = ReadFile(PluginFileSeek, SelectedData);
 
-		std::string EndInfo;
+			std::cout << "Readed: " << std::hex << ReadedData << std::endl;
 
-		//Checking
-		//ReadFile
-		FILE *WriteData;
-		FILE *ReadData;
-		int DataBuf;	//Data Buffer
-		ReadData = fopen(SelectedData, "rb");
-	
-
-		if (!ReadData) {
-			std::cout << "Failed to Open Data!\n";
+		if (!inRange(MinimumValue, MaximumValue, ReadedData))
+		{
+			WriteFile(PluginFileSeek, SelectedData, NewValue);
+			EndInfo = "Plugin ended, changed in Seek " + std::to_string(PluginFileSeek) + " the Value: " + std::to_string(NewValue) + ".";
 		}
-		else {
-			fseek(ReadData, PluginFileSeek, SEEK_SET);
-			fread(&DataBuf, sizeof(DataBuf), 1, ReadData);
-			fclose(ReadData);
+		else
+		{
+			EndInfo = "Plugin ended and changed nothing. (No Crashes found!)";
 		}
+		MessageBox(nullptr, EndInfo.c_str(), "Plugin", MB_ICONINFORMATION);
+}
 
-		std::cout << "Reading: " <<std::hex << DataBuf << std::endl;
-
-			if(!inRange(MinimumValue, MaximumValue, DataBuf))
-			{
-				std::cout << "Writing file...\n";
-				WriteData = fopen(SelectedData, "rb+");
-				if (!WriteData)
-				{
-					std::cout << "File didn't exists!\n";
-				}
-				else
-				{
-					fseek(WriteData, PluginFileSeek, SEEK_SET);
-					fwrite(&NewValue, sizeof(NewValue), 1, WriteData);
-					std::cout << "Plugin: Ended. changed in Seek " << std::hex << PluginFileSeek << " the Value: " << NewValue << std::endl;
-					EndInfo = "Plugin ended, changed in Seek " + std::to_string(PluginFileSeek) +  " the Value: " + std::to_string(NewValue) +  ".";
-						MessageBox(nullptr, EndInfo.c_str(), "Plugin", MB_ICONINFORMATION);
-					fclose(WriteData);
-				}
-
-			
-			}
-
-			else
-			{
-
-				EndInfo = "Plugin ended and changed nothing.";
-				MessageBox(nullptr, EndInfo.c_str(), "Plugin", MB_ICONINFORMATION);
-			}
-		//Write File
-		
-			
-	}
 
 
 
@@ -79,64 +45,80 @@ namespace Plugin
 		int NewValue = 0;
 
 		std::cout << "***Plugin Loader v0.1***\n\n";
-		
-			std::ifstream in(path);
 
-			if (!in.is_open())
+		std::ifstream in(path);
+
+		if (!in.is_open())
+		{
+			std::cout << "Cannot Open the Plugin file.";
+			return false;
+		}
+
+		std::string param;
+		std::string value;
+
+
+		while (!in.eof())
+		{
+			in >> param;
+			in >> value;
+
+			if (param == "CREATOR")
 			{
-				std::cout << "Cannot Open the Plugin file.";
-				return false;
+				PluginCreatorName = value;
 			}
-
-			std::string param;
-			std::string value;
-			
-			
-			while (!in.eof())
-			{
-				in >> param;
-				in >> value;
-
-				if (param == "CREATOR")
-				{
-					PluginCreatorName = value;
-				}
-				else if (param == "SEEK")
-				{
-					sscanf(value.c_str(), "%x", &PluginFileSeek);
-				}
-				else if (param == "CRASH_VALUE_MIN")
-					sscanf(value.c_str(), "%x", &MinimumValue);
-				else if (param == "CRASH_VALUE_MAX")
-					sscanf(value.c_str(), "%d", &MaximumValue);
-				else if (param == "NEW_VALUE")
-					sscanf(value.c_str(), "%d", &NewValue);
-				else if (param == "NOTE")
-					std::getline(in, PluginNote);
-			
-
-			}
-
-		
+			else if (param == "SEEK")
+				sscanf(value.c_str(), "%x", &PluginFileSeek);
+			else if (param == "CRASH_VALUE_MIN")
+				sscanf(value.c_str(), "%d", &MinimumValue);
+			else if (param == "CRASH_VALUE_MAX")
+				sscanf(value.c_str(), "%d", &MaximumValue);
+			else if (param == "NEW_VALUE")
+				sscanf(value.c_str(), "%d", &NewValue);
+			else if (param == "NOTE")
+				std::getline(in, PluginNote);
 
 
-			MessageBox(nullptr,  PluginCreatorName.c_str(), "Plugin Creator", MB_ICONINFORMATION);
-			MessageBox(nullptr, PluginNote.c_str(), "Note", MB_ICONINFORMATION);
-			// Log the loaded configuration
+		}
+
+		std::cout << "Seek:" <<PluginFileSeek << std::endl;
+		std::cout << "CRASH_VALUE_MIN:" << MinimumValue << std::endl;
+		std::cout << "CRASH_VALUE_MAX:" << MaximumValue << std::endl;
+		std::cout << "NEW_VALUE:" << NewValue << std::endl;
+		std::cout << "NOTE:" << PluginNote << std::endl;
+
+		MessageBox(nullptr, PluginCreatorName.c_str(), "Plugin Creator", MB_ICONINFORMATION);
+		MessageBox(nullptr, PluginNote.c_str(), "Note", MB_ICONINFORMATION);
+		// Log the loaded configuration
 
 
-			Plugin::WriteAndReadFromPlugin(PluginFileSeek, MinimumValue, MaximumValue, NewValue, SelectedData);
+		Plugin::WriteAndReadFromPlugin(PluginFileSeek, MinimumValue, MaximumValue, NewValue, SelectedData);
 
-			in.close();
+		in.close();
 
-			return true;
-		
+		return true;
+
 	}
 
 	void Load(const char SelectedData[6])
 	{
 		// MessageBox(nullptr, "Unknown Plugins can corrupt your save File!", "Warning", MB_ICONWARNING);
-		Read(FileDialog::Open(), SelectedData);
+		char path[1024];
+		path[0] = 0;
+		OPENFILENAME LoadDL = {};
+		LoadDL.lStructSize = sizeof(OPENFILENAME);
+		LoadDL.lpstrFilter = "ZPlugin-Files (.Zplg)\0*.Zplg;\0All Files\0*.*";
+		LoadDL.lpstrCustomFilter = 0;
+		LoadDL.nMaxCustFilter = 0;
+		LoadDL.nFilterIndex = 0;
+		LoadDL.lpstrFile = path;
+		LoadDL.nMaxFile = 256;
+		LoadDL.nMaxFileTitle = 1024;
+		LoadDL.lpstrTitle = "Select a Plugin to load";
+		LoadDL.Flags = OFN_HIDEREADONLY | OFN_NOCHANGEDIR;
+	
+		if (GetOpenFileName(&LoadDL) != 0)
+			Read(path, SelectedData);
 	}
 
 
