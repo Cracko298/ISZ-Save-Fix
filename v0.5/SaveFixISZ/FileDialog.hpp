1810f1bdf4e99813b43b59d6b3aa9723c31e19d8
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2024-04-11 10:44:43 -0400

    Delete v0.5 directory

--- v0.5/SaveFixISZ/FileDialog.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <iostream>
-
-namespace FileDialog
-{
-	std::string Open();
-	void Save();
-};
--- v0.5/SaveFixISZ/Plugin.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <windows.h>
-
-namespace Plugin
-{
-	void Load(const char SelectedData[6]);
-};
